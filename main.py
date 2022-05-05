5350de80bc00458caf902b20ce3f2778972a3986
MaksimUrusov <den-ney@bk.ru>
2022-05-06 02:36:45 +0300

    test7

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-var_1 = [1, 5, 6, 7]
-print(eval(str(var_1).replace(', ', '+'))[0]/len(var_1))
-# Вывод:
+# The user's inputs for the numbers and the operators
+num1 = float(input('Enter your first number: '))
+Operator = input('Enter operator: ')
+num2 = float(input('Enter your second number: '))
+
+# if Operator is (+ | - | * | /) then  print out number 1 (+ | - | * | /) number 2
+if Operator == '+':
+    print(num1 + num2)
+elif Operator == '-':
+    print(num1 - num2)
+elif Operator == '/':
+    print(num1 / num2)
+elif Operator == '*':
+    print(num1 * num2)
+
+# if the user didn't put an operator
+else:
+    print('Not a valid operator')
